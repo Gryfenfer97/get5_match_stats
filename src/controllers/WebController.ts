3333ba063bced8626b2b0de94f9c14e8e62fe607
Gryfenfer97 <hajjarjamil4@gmail.com>
2022-11-07 23:30:22 +0100

    connect to the database

--- src/controllers/WebController.ts
+++ src/controllers/WebController.ts
@@ -1,41 +1,23 @@
 import { Request, Response, Router } from 'express'
-import { getMatchs } from '../modules/match'
+import { getMatchs, getMaps, getMap } from '../modules/match'
 
 export default function routes(router: Router) {
 	router.get('/', serveHomePage)
 	router.get('/match/:id', serveMatchPage)
-	router.get('/match/:id/map/:id', serveMapPage)
+	router.get('/match/:matchid/map/:mapnumber', serveMapPage)
 }
 
 async function serveHomePage(_req: Request, res: Response) {
-	console.log(await getMatchs())
-	const matchsData = [
-		{id: 1, team1: 'Team 1', team2: 'Team 2', team1_score: 2, team2_score: 1, series_type: 'bo3'},
-		{id: 2, team1: 'Team 3', team2: 'Team 4', team1_score: 0, team2_score: 2, series_type: 'bo3'},
-	]
-	res.render('home.ejs', {matches: matchsData})
+	res.render('home.ejs', {matches: await getMatchs()})
 
 }
 
 async function serveMatchPage(req: Request, res: Response) {
-	const matchData = { matchid: 1, team1: 'Team 1', team2: 'Team 2', team1Score: 2, team2Score: 1, maps: [
-		{ mapName: 'de_dust2', team1Score: 16, team2Score: 14, duration: '30:32', mapNumber: 1 },
-		{ mapName: 'de_inferno', team1Score: 13, team2Score: 16, duration: '27:19', mapNumber: 2 },
-		{ mapName: 'de_mirage', team1Score: 16, team2Score: 15, duration: '40:46', mapNumber: 3 }] }
+	const matchData = await getMaps(parseInt(req.params.id))
 	res.render('match.ejs', matchData)
 }
 
 async function serveMapPage(req: Request, res: Response) {
-	const mapData = {
-		team1: 'team1', team2: 'team2', mapName: 'de_dust2', team1_score: 16, team2_score: 14,
-		players: [
-			{name: 'player1', steamid: '76561197960287930', kills: 16, assists: 17, deaths: 19, team: 'team1'},
-			{name: 'player2', steamid: '76561197960287931', kills: 13, assists: 10, deaths: 14, team: 'team1'},
-			{name: 'player3', steamid: '76561197960287932', kills: 16, assists: 17, deaths: 19, team: 'team1'},
-			{name: 'player4', steamid: '76561197960287933', kills: 16, assists: 17, deaths: 19, team: 'team1'},
-			{name: 'player5', steamid: '76561197960287934', kills: 16, assists: 17, deaths: 19, team: 'team1'}
-
-		]
-	}
-	res.render('map.ejs', mapData)
+	const match = await getMap(parseInt(req.params.matchid), parseInt(req.params.mapnumber))
+	res.render('map.ejs', match)
 }
--- src/modules/match.ts
+++ src/modules/match.ts
@@ -2,14 +2,25 @@ import db from './database'
 
 export async function getMatchs(){
     const matchList = (await db.query('SELECT * FROM get5_stats_matches'))[0]
-
     return matchList
 }
 
-export async function getMaps(matchid: string){
-    const mapList = (await db.execute('SELECT * FROM get5_get_maps WHERE matchid=?', matchid))[0] as [any]
+export async function getMaps(matchid: number){
+    const mapList = (await db.execute('SELECT * FROM get5_stats_maps WHERE matchid=?', [matchid]))[0] as [any]
+    let match = ((await db.execute('SELECT * FROM get5_stats_matches WHERE matchid=?', [matchid]))[0] as [any])[0] as any
     mapList.forEach(map => {
-        map.duration = map.end_time - map.start_time //TODO: check if it works with the database result
+        map.duration = Math.round((map.end_time - map.start_time) / 60000)
     });
-    return mapList
+    match.maps = mapList
+    return match
+}
+
+export async function getMap(matchid: number, mapnumber: number){
+    const players = (await db.execute('SELECT * FROM get5_stats_players WHERE matchid=? AND mapnumber=?', [matchid, mapnumber]))[0] as [any]
+    const map = ((await db.execute('SELECT * FROM get5_stats_maps WHERE matchid=? AND mapnumber=?', [matchid, mapnumber]))[0] as any)[0]
+    let match = ((await db.execute('SELECT * FROM get5_stats_matches WHERE matchid=?', [matchid]))[0] as [any])[0]
+    map.duration = Math.round((map.end_time - map.start_time) / 60000)
+    match.map = map;
+    match.players = players;
+    return match;
 }
